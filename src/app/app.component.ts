3668efa2cc2138b1ee53f7561ce7049e9bd4255a
Ahsan Imtiaz <ahsan.imtiazrafiq@systemsltd.com>
2021-01-15 21:38:13 +0500

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'static-web';
+  slides = [
+    { img: "../assets/images/main-banner-1.png" },
+    { img: "../assets/images/main-banner-2.png" }
+  ];
+  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1 };
+
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 }
+
