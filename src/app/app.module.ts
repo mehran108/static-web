a9807d23e911daeeaae8d089a958f02b1fc737e6
Mehran Khan <mehran.khan@systemsltd.com>
2021-01-21 12:34:42 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThreeSixtyModule } from '@mediaman/angular-three-sixty';
+import { AboutComponent } from 'src/components/about/about.component';
+import { ContactComponent } from 'src/components/contact/contact.component';
+import { DealershipComponent } from 'src/components/dealership/dealership.component';
+import { LifestyleComponent } from 'src/components/lifestyle/lifestyle.component';
+import { ProjectOverviewComponent } from 'src/components/project-overview/project-overview.component';
+import { ServiceComponent } from 'src/components/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    DealershipComponent,
+    LifestyleComponent,
+    ProjectOverviewComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ThreeSixtyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
