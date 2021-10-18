4f6927521cec1cc4b2c846bcb7ad6a065c433886
Muhammadkhizar <khizeraziz786@gmail.com>
2021-10-19 00:44:07 +0500

    delete old project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from 'src/components/about/about.component';
-import { ContactComponent } from 'src/components/contact/contact.component';
-import { DealershipComponent } from 'src/components/dealership/dealership.component';
-import { LifestyleComponent } from 'src/components/lifestyle/lifestyle.component';
-import { ProjectOverviewComponent } from 'src/components/project-overview/project-overview.component';
-import { ServiceComponent } from 'src/components/service/service.component';
-import { HomeComponent } from 'src/components/home/home.component';
-import { ProjectAlraziqComponent } from 'src/components/project-alraziq/project-alraziq.component';
-import { ProjectUpcomingComponent } from 'src/components/project-upcoming/project-upcoming.component';
-import { PanoramaImageRendererComponent } from 'src/components/project-overview/panorama-image-renderer/panorama-image-renderer.component';
-import { UpcomingKallarKaharComponent } from 'src/components/upcoming-kallar-kahar/upcoming-kallar-kahar.component';
-import { UpcomingSupremeCityComponent } from 'src/components/upcoming-supreme-city/upcoming-supreme-city.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'dealership', component: DealershipComponent },
-  { path: 'lifestyle', component: LifestyleComponent },
-  { path: 'project', component: ProjectOverviewComponent },
-  { path: 'service', component: ServiceComponent },
-  { path: 'alraziq', component: ProjectAlraziqComponent },
-  { path: 'upcoming', component: ProjectUpcomingComponent },
-  { path: 'panorama', component: PanoramaImageRendererComponent },
-  { path: 'kallar-kahar', component: UpcomingKallarKaharComponent },
-  { path: 'supreme-city', component: UpcomingSupremeCityComponent }
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'dealership', component: DealershipComponent },
+  // { path: 'lifestyle', component: LifestyleComponent },
+  // { path: 'project', component: ProjectOverviewComponent },
+  // { path: 'service', component: ServiceComponent },
+  // { path: 'alraziq', component: ProjectAlraziqComponent },
+  // { path: 'upcoming', component: ProjectUpcomingComponent },
+  // { path: 'panorama', component: PanoramaImageRendererComponent },
+  // { path: 'kallar-kahar', component: UpcomingKallarKaharComponent },
+  // { path: 'supreme-city', component: UpcomingSupremeCityComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,53 +3,16 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from 'src/components/about/about.component';
-import { ContactComponent } from 'src/components/contact/contact.component';
-import { DealershipComponent } from 'src/components/dealership/dealership.component';
-import { LifestyleComponent } from 'src/components/lifestyle/lifestyle.component';
-import { ProjectOverviewComponent } from 'src/components/project-overview/project-overview.component';
-import { ServiceComponent } from 'src/components/service/service.component';
-import { HeaderComponent } from 'src/components/header/header.component';
-import { HomeComponent } from 'src/components/home/home.component';
-import { ProjectAlraziqComponent } from 'src/components/project-alraziq/project-alraziq.component';
-import { ProjectUpcomingComponent } from 'src/components/project-upcoming/project-upcoming.component';
-import { LightboxModule } from 'ngx-lightbox';
-import { PanoramaImageRendererComponent } from 'src/components/project-overview/panorama-image-renderer/panorama-image-renderer.component';
-import { NgxCaptchaModule } from 'ngx-captcha';
-import { ThreeSixtyModule } from '@mediaman/angular-three-sixty';
-import { FooterComponent } from 'src/components/footer/footer.component';
-import { UpcomingKallarKaharComponent } from 'src/components/upcoming-kallar-kahar/upcoming-kallar-kahar.component';
-import { UpcomingSupremeCityComponent } from 'src/components/upcoming-supreme-city/upcoming-supreme-city.component';
-import { NgxImageZoomModule } from 'ngx-image-zoom';
 // import third-party module
 import { AnimateOnScrollModule } from 'ngx-animate-on-scroll';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    ContactComponent,
-    DealershipComponent,
-    LifestyleComponent,
-    ProjectOverviewComponent,
-    ServiceComponent,
-    HeaderComponent,
-    HomeComponent,
-    PanoramaImageRendererComponent,
-    ProjectAlraziqComponent,
-    ProjectUpcomingComponent,
-    FooterComponent,
-    UpcomingKallarKaharComponent,
-    UpcomingSupremeCityComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ThreeSixtyModule,
-    LightboxModule,
-    NgxCaptchaModule,
-    AnimateOnScrollModule.forRoot(),
-    NgxImageZoomModule // <-- Add this line
+    AppRoutingModule
 
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
